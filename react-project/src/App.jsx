512ab9e3daf72535482e98e0805d022ce56ccf1b
md sinikdho <mdsinikdho94@gmail.com>
2025-11-05 00:19:50 +0600

    conditional rendaring

---
+++ react-project/src/App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+
+function Item({ name, isPacked }) {
+  // controller/togical part
+  const isCheckMark = isPacked ? "✅" : null;
+
+  // Logical or oparator
+  //  const isCheckMark = isPacked && "✅" ;
+
+  // view
+  return (
+    <li className="item">
+      {" "}
+      {name} {isCheckMark}
+    </li>
+  );
+}
+
+function App() {
+  const pasckingList = [
+    {
+      id: 1,
+      name: "sinikdho ",
+      isPacked: true,
+    },
+    {
+      id: 2,
+      name: "sinikdho ",
+      isPacked: true,
+    },
+    {
+      id: 3,
+      name: "sinikdho ",
+      isPacked: false,
+    },
+  ];
+  return (
+    <>
+      <h1> Conditional Rendering</h1>
+      <ul>
+        {pasckingList.map((list) => (
+          <Item name={list.name} isPacked={list.isPacked} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ react-project/src/Components/RenderingLists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RenderingLists() {
+  return (
+    <section>
+      <h2>Rendering Lists</h2>
+    </section>
+  );
+}
+
+export default RenderingLists;
